44aae7873169c8e00cbb485d75c1a415e9786b89
Bartosz Kazuła <bartosz@kazula.eu>
2024-05-17 02:21:09 +0200

    Ignore image assets

--- lib/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:ui';
-
-const Color textColor = Color.fromRGBO(25, 25, 24, 1);
-const Color primaryColor = Color(0xFFE94190);
-const Color secondaryColor = Color(0xFF3463AC);
-const Color selectedColor = Color(0xFFFFD143);

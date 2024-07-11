496122b3e1da1517fe3578098f285444700960dd
Bartosz Kazuła <bartosz@kazula.eu>
2024-07-11 02:07:51 +0200

    Prepare for release of two independent apps

--- lib/client.dart
+++ lib/client.dart
@@ -2,6 +2,6 @@ import 'package:flutter_sanity_client/flutter_sanity_client.dart';
 
 final sanityClient = SanityClient(
   dataset: 'production',
-  projectId: 'q5zach5q',
+  projectId: const String.fromEnvironment('SANITY_PROJECT_ID'),
   apiVersion: 'v2022-03-07',
 );

7376b371bd7b39dbbfb1bc933f99cc741036a3de
Bartosz Kazuła <bartosz@kazula.eu>
2024-08-09 13:50:28 +0200

    New icons

--- lib/pages/map_screen.dart
+++ lib/pages/map_screen.dart
@@ -90,6 +90,22 @@ class MapScreenState extends State<MapScreen> implements TickerProvider {
         return Icons.directions_train_sharp;
       case 'bus':
         return Icons.directions_bus_sharp;
+      case 'restaurant':
+        return Icons.restaurant;
+      case 'counter_1':
+        return Icons.looks_one_outlined;
+      case 'counter_2':
+        return Icons.looks_two_outlined;
+      case 'counter_3':
+        return Icons.looks_3_outlined;
+      case 'hotel':
+        return Icons.hotel;
+      case 'sports_soccer':
+        return Icons.sports_soccer;
+      case 'sports_basketball':
+        return Icons.sports_basketball;
+      case 'local_cafe':
+        return Icons.local_cafe;
       case 'pin':
       default:
         return Icons.pin_drop_outlined;

d78668735f52c929b0d565ff01088930e26dc9a0
Bartosz Kazuła <bartosz@kazula.eu>
2025-03-09 22:04:27 +0100

    Extract info tile as a separate component

--- lib/pages/information_screen.dart
+++ lib/pages/information_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../widgets/momentum_appbar.dart';
+import '../widgets/info_tile.dart';
 import './information/regulations_screen.dart';
 import './information/songs_screen.dart';
 import './information/speakers_screen.dart';
@@ -16,8 +17,7 @@ class InformationScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          _buildInfoTile(
-            context: context,
+          InfoTile(
             title: AppLocalizations.of(context)!.speakers,
             image: 'assets/images/mowcy.jpg',
             onTap: () => Navigator.push(
@@ -25,15 +25,13 @@ class InformationScreen extends StatelessWidget {
                 MaterialPageRoute(
                     builder: (context) => const SpeakersScreen())),
           ),
-          _buildInfoTile(
-            context: context,
+          InfoTile(
             title: AppLocalizations.of(context)!.songs,
             image: 'assets/images/teksty.jpg',
             onTap: () => Navigator.push(context,
                 MaterialPageRoute(builder: (context) => const SongsScreen())),
           ),
-          _buildInfoTile(
-            context: context,
+          InfoTile(
             title: AppLocalizations.of(context)!.regulations,
             image: 'assets/images/regulamin.jpg',
             onTap: () => Navigator.push(
@@ -42,61 +40,6 @@ class InformationScreen extends StatelessWidget {
                     builder: (context) => const RegulationsScreen())),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildInfoTile(
-      {required BuildContext context,
-      required String title,
-      required String image,
-      required VoidCallback onTap}) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          margin: const EdgeInsets.only(top: 2.5, bottom: 2.5),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: AspectRatio(
-                    aspectRatio: 16 / 9,
-                    child: Stack(
-                      children: [
-                        Image.asset(
-                          "/$image",
-                          fit: BoxFit.contain,
-                        ),
-                        Positioned(
-                          bottom: 10,
-                          right: 10,
-                          child: Text(
-                            title,
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).brightness ==
-                                      Brightness.dark
-                                  ? Theme.of(context).colorScheme.onSurface
-                                  : Theme.of(context).colorScheme.surface,
-                              shadows: const <Shadow>[
-                                Shadow(
-                                  offset: Offset(1.0, 1.0),
-                                  blurRadius: 3.0,
-                                  color: Colors.black,
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  )),
-            ],
-          ),
-        ),
       ),
     );
   }
